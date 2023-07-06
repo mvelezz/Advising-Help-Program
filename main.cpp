a0b726a46a0f40c28c42ebfb3214cbc607133096
mvelezz <130081489+mvelezz@users.noreply.github.com>
2023-07-06 10:43:00 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,72 @@
 #include <iostream>
-#include "Header.h"
+#include<vector>
+#include<string>
+#include<iostream>
+#include<fstream>
+#include<sstream>
+using namespace std;
+
+//course class
+class courseSchedule {
+public:
+	std::string courseID, courseName, preRequisite1, preRequisite2;
+	void input(std::string line);
+ 
+};
+
+//function to allow all types of user input
+string makeUpper(string str) {
+	string result = "";
+	for (int i = 0; i < str.size(); ++i) {
+		result += toupper(str[i]);
+	}
+	return result;
+}
+
+
+//reading the file line by line, seperated by commas
+void courseSchedule::input(std::string line) {
+	stringstream ss(line);
+
+	getline(ss, courseID, ',');
+	if (!getline(ss, courseName, ',')) {
+		std::cout << "Error: missing name of the course id " << courseID << std::endl;
+		exit(0);
+	}
+	if (!getline(ss, preRequisite1, ',')) {
+		preRequisite1 = "";
+
+	}
+	if (!getline(ss, preRequisite2, ',')) {
+		preRequisite2 = "";
+	}
+}
+
+
+//selection sort to sort the courses alphabetically
+void selectionSort(vector<courseSchedule>& course) {
+	int smallest;
+	for (int i = 0; i < course.size(); ++i) {
+		smallest = i;
+		for (int j = i + 1; j < course.size(); j++) {
+			if (course[j].courseName.compare(course[smallest].courseName) < 0) {
+				smallest = j;
+			}
+		}
+		if (smallest != i)
+			swap(course[i], course[smallest]);
+	}
+}
+
 
 int main() {
 
+	//create course objects
 	courseSchedule course;
 	std::vector<courseSchedule> courses;
 
 	int userInput = 0;
+	string courseSelection;
 
 	std::cout << "MENU" << std::endl;
 	std::cout << "CHOICE 1: LOAD DATA" << std::endl;
@@ -16,37 +76,70 @@ int main() {
 
 	std::cin >> userInput;
 
-	while (userInput == 1 || userInput == 2 || userInput == 3 || userInput == 4) {
+
+	//take userinput when userinput is not 4
+	while (userInput != 4) {
+		if (userInput != 1 && userInput != 2 && userInput != 3 && userInput != 4) {
+			cout << "Invalid input" << endl;
+			cin.clear();
+			cin.ignore(1000000, '\n');
+		}
 		switch (userInput) {
+		//load data into the data structure froma file
 		case 1:
-		{std::cout << "Made it to 1" << std::endl;
+		{ 
 		std::string line;
 		std::ifstream fileObj;
-		std::cout << "Opening File. " << std::endl;
-		fileObj.open("Courses.txt");
+		string userFile;
+		std::cout << "Please enter the file you would like to open." << std::endl;
+		std::cin >> userFile;
+		fileObj.open(userFile);
 		if (!fileObj.is_open()) {
-			std::cout << "Could not open file";
+			std::cout << "Could not open file" << endl;
+			break;
 			return 1;
+			
 		}
 		else {
+			//add objects to a vector
 			std::cout << "Reading from file" << std::endl;
 			while (fileObj.good()) {
 				getline(fileObj, line);
 				courseSchedule course1;
 				course1.input(line);
 				courses.push_back(course1);
+
+
 			}
+			fileObj.close();
 		}
+		cout << "File has been loaded" << endl;
+
 		break;
 		}
 		case 2:
-			std::cout << "made it to 2" << std::endl;
+			//sort and print out all courses in the data structure
+			selectionSort(courses);
+			for (const auto& course : courses) {
+				cout << course.courseID << endl;
+				cout << course.courseName << endl;
+				cout << endl;
+			}
 			break;
 		case 3:
-			std::cout << "made to 3" << std::endl;
+			//provide info based on certain courses
+			cout << "What course would you like to know about?" << endl;
+			cin >> courseSelection;
+
+			//loop through data strucutre based on course selected and print prerequisites
+			for (int i = 0; i < courses.size(); i++) {
+				if (makeUpper(courseSelection) == courses[i].courseID) {
+					cout << courses[i].courseName << endl;
+					cout << "Prerequisites: " << courses[i].preRequisite1 << ", " << courses[i].preRequisite2 << endl;
+				}
+			}
 			break;
 		case 4:
-			std::cout << "made it to 4" << std::endl;
 			break;
 
 		}
@@ -57,25 +150,9 @@ int main() {
 		std::cout << "CHOICE 4: EXIT" << std::endl;
 		std::cin >> userInput;
 
-		while (userInput != 1 && userInput != 2 && userInput != 3 && userInput != 4) {
-			std::cout << "INVALID INPUT" << std::endl;
-			std::cout << "MENU" << std::endl;
-			std::cout << "CHOICE 1: LOAD DATA" << std::endl;
-			std::cout << "CHOICE 2: PRINT COURSE LIST" << std::endl;
-			std::cout << "CHOICE 3: PRINT COURSE ALONG WITH PREREQUISITES FOR THAT COURSE" << std::endl;
-			std::cout << "CHOICE 4: EXIT" << std::endl;
-			std::cin >> userInput;
-		}
+	}
 
-	}
-	while (userInput != 1 && userInput != 2 && userInput != 3 && userInput != 4) {
-		std::cout << "INVALID INPUT" << std::endl;
-		std::cout << "MENU" << std::endl;
-		std::cout << "CHOICE 1: LOAD DATA" << std::endl;
-		std::cout << "CHOICE 2: PRINT COURSE LIST" << std::endl;
-		std::cout << "CHOICE 3: PRINT COURSE ALONG WITH PREREQUISITES FOR THAT COURSE" << std::endl;
-		std::cout << "CHOICE 4: EXIT" << std::endl;
-		std::cin >> userInput;
-	}
 
 }
+
+
